62760a7333f8b7a93c666514429efadbca7534b9
LiDongdong20170118 <35412957+LiDongdong20170118@users.noreply.github.com>
2018-04-02 11:28:58 +0800

    Common Function of Python
    My Common Function of Python

---
+++ Common.py
@@ -0,0 +1,13 @@
+import math
+import numpy as npy
+#测试函数
+def Test():
+    print("Dongdong")
+
+#求两个变量的平方和的根
+def SolvingDis(a,b):
+    print(math.sqrt(a*a+b*b))
+
+
+# A   =   npy.array(range(1, 10))
+# print(A)
